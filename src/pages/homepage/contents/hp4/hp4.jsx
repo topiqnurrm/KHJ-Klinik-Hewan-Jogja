efb5545413964264b386168daddf11e62b55d017
toopique <sossmeed@gmail.com>
2025-03-05 10:21:23 +0700

    update

--- src/pages/homepage/contents/hp4/hp4.jsx
+++ src/pages/homepage/contents/hp4/hp4.jsx
@@ -9,10 +9,10 @@ function Hp4() {
       <div className="content-wrapper">
         {/* Tombol Navigasi */}
         <nav className="navigation">
-          <button onClick={() => setActivePage("page1")}>Page 1s</button>
-          <button onClick={() => setActivePage("page2")}>Page 2d</button>
-          <button onClick={() => setActivePage("page3")}>Page 3g</button>
-          <button onClick={() => setActivePage("page4")}>Page 4f</button>
+          <button onClick={() => setActivePage("page1")}>Page 1</button>
+          <button onClick={() => setActivePage("page2")}>Page 2</button>
+          <button onClick={() => setActivePage("page3")}>Page 3</button>
+          <button onClick={() => setActivePage("page4")}>Page 4</button>
         </nav>
 
         {/* Halaman yang Ditampilkan */}
