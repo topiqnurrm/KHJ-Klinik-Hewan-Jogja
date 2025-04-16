ba9bafc904dd720edaa5a6415f17c1ee58352755
toopique <sossmeed@gmail.com>
2025-04-16 17:28:12 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  // <== Tambahkan ini
 import './main.css'
-import Userland from './show_pages/homepage/homepage.jsx'
-import Login from './show_pages/loginkhj/loginkhj.jsx'
-
-const user = JSON.parse(localStorage.getItem("user"))
+import Homepage from './show_pages/homepage/homepage.jsx'
+import Loginkhj from './show_pages/loginkhj/loginkhj.jsx'
+import Adminkhj from './show_pages/adminkhj/adminkhj.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      {<Userland />}
-    </BrowserRouter>
+    <Loginkhj />
   </StrictMode>,
 )
